93f48c37c4f17ce1ce0030ac430ee5ea2b3feba5
maneulyori <maneulyori@gmail.com>
2012-10-10 11:38:29 +0900

    maxima module.

---
+++ modules/maxima.js
@@ -0,0 +1,37 @@
+var spawn = require('child_process').spawn;
+
+var client;
+
+exports.init = init;
+exports.messageHandler = messageHandler;
+
+var maxima = spawn('maxima', []);
+
+function init (initArg)
+{
+	client = initArg.client;
+
+	return { moduleCommand: { command: ["maxima"] }, callBack: messageHandler, unloadCallback: unload };
+}
+
+function messageHandler(message)
+{
+	if(message.splitedMessage[0] == "maxima")
+	{
+		if(message.splitedMessage[1] == "재시동")
+		{
+			client.privmsg(message.args[0], "Restarting maxima process...");
+			maxima.kill('SIGKILL');
+			maxima = spawn('maxima', []);
+			client.privmsg(message.args[0], "Restarting finished.");
+		}
+
+
+		client.privmsg (message.args[0], "Placeholder!");
+	}
+}
+
+function unload()
+{
+	maxima.kill('SIGKILL');
+}
