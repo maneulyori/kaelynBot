b92083f641dbcebd6dc335e8542717e2fcd1e9e4
maneulyori <maneulyori@gmail.com>
2012-12-31 22:15:25 +0900

    Update of donate.js module

--- modules/donate.js
+++ modules/donate.js
@@ -14,6 +14,7 @@ function messageHandler(message)
 {
 	if(message.splitedMessage[0] == "마늘기부")
 	{
-		client.privmsg (message.channel, "좀비에게 카페인을 BTC wallet: 17WZTVMUCfkCJnL4593DfvQ7eBkAqAj9N2");
+		client.privmsg (message.channel, "좀비에게 카페인을 하나은행 725-910132-94707 PayPal: maneulyori@gmail.com");
+		client.privmsg (message.channel, "BTC wallet: 17WZTVMUCfkCJnL4593DfvQ7eBkAqAj9N2");
 	}
 }
