32907b3626104bfea32d42c46ad02601b6416f01
maneulyori <maneulyori@gmail.com>
2013-01-15 03:28:07 +0900

    Add exception for throw null;

--- modules/eval/worker.js
+++ modules/eval/worker.js
@@ -14,7 +14,11 @@ process.on('message', function(msg) {
 	}
 	catch (e)
 	{
-		if(typeof(e) != 'undefined')
+		if(e == null)
+		{
+			sandbox.out = "에러 던질거면 제대로 던져 ㅗ null 던져서 뭐 하겠다고?";
+		}
+		else if(typeof(e) == 'undefined')
 		{
 			if(typeof(e.name) != 'undefined' && typeof(e.message) != 'undefined')
 				sandbox.out = e.name + ": " + e.message;
@@ -22,7 +26,9 @@ process.on('message', function(msg) {
 				sandbox.out = "에러 던질거면 제대로 던져 ㅗ"
 		}
 		else
+		{
 			sandbox.out = "에러 던질거면 제대로 던져 ㅗ undefined 던져서 뭐 하겠다고?";
+		}
 	}
 
 	var outArr = sandbox.out.split("\n");
