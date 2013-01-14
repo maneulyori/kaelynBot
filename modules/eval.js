a2dff551b9ce519da4eb5e914ebfc8dce28df726
maneulyori <maneulyori@gmail.com>
2013-01-15 01:22:43 +0900

    eval.js bugfix

--- modules/eval.js
+++ modules/eval.js
@@ -5,8 +5,6 @@ var client;
 exports.init = init;
 exports.messageHandler = messageHandler;
 
-var whitelist = ["마늘!garlic@elrond.maneulyori.org"];
-
 function init (initArg)
 {
 	client = initArg.client;
@@ -18,20 +16,21 @@ function messageHandler(message)
 {
 	if(message.moduleCommand == "eval")
 	{
+			/*
 		for (var nick in whitelist)
 		{
 			if(message.prefix == whitelist[nick])
-			{
+			{*/
 				var sandbox = {
 					out : "",
 				}
 				var source = message.content.match(/([^\s]+)\ (.+)/);
-				var compiledScript = vm.createScript(source);
+				var compiledScript = vm.createScript(source[2]);
 
 				compiledScript.runInNewContext(sandbox);
 
 				client.privmsg(message.channel, sandbox.out);
-			}
-		}
+			//}
+		//}
 	}
 }
