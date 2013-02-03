71a8f9ab401bc7bb275b4fbf4e9f7ef83c997f11
maneulyori <maneulyori@gmail.com>
2013-02-03 21:22:01 +0900

    improved donation module

--- modules/donate.js
+++ modules/donate.js
@@ -7,14 +7,11 @@ function init (initArg)
 {
 	client = initArg.client;
 
-	return { moduleCommand: {command: ["마늘기부"]}, callBack: messageHandler };
+	return { moduleCommand: {command: ["마늘기부", "기부"]}, callBack: messageHandler };
 }
 
 function messageHandler(message)
 {
-	if(message.splitedMessage[0] == "마늘기부")
-	{
-		client.privmsg (message.channel, "좀비에게 카페인을 하나은행 725-910132-94707 PayPal: maneulyori@gmail.com");
-		client.privmsg (message.channel, "BTC wallet: 17WZTVMUCfkCJnL4593DfvQ7eBkAqAj9N2");
-	}
+	client.privmsg (message.channel, "좀비에게 카페인을 하나은행 725-910132-94707 PayPal: maneulyori@gmail.com");
+	client.privmsg (message.channel, "BTC wallet: 17WZTVMUCfkCJnL4593DfvQ7eBkAqAj9N2");
 }
